1a261e96d321014244b2e511c063da71c9113690
Kyle King <100040199@ccisd.net>
2021-01-29 15:22:42 -0600

    read up on interfaces and inheritance for C#

--- Assets/Scripts/EnemyScript/EnemyMoveTypes/BaseEnemyMove.cs
+++ Assets/Scripts/EnemyScript/EnemyMoveTypes/BaseEnemyMove.cs
@@ -9,4 +9,6 @@ public abstract class BaseEnemyMove : ScriptableObject
     public abstract Vector2 Move(float spd);
 
     public abstract float MakeAngle();
+
+    //The modifier 'virtual' is not valid for this item
 }
--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -5,22 +5,26 @@ using UnityEngine.UI;
 
 public class StartScene : MonoBehaviour
 {
-    public int x, y;
+    //public int x, y;
 
-    //scene specific enemies, but always have "Nothing" at the end
-    public GameObject[] en;
+    ////scene specific enemies, but always have "Nothing" at the end
+    //public GameObject[] en;
 
-    /* 0 = RETURN, 1 = CHASE, 2 = NONE
-         * 3 = LINEAR, 4 = DIAGONAL, 5 = FREEFORM
-         * 6 = SPORADIC, 7 = PATROL, 8 = TELEPORT
-         */
-    public int[] mT;
-    public Texture[] bg;
+    ///* 0 = RETURN, 1 = CHASE, 2 = NONE
+    //     * 3 = LINEAR, 4 = DIAGONAL, 5 = FREEFORM
+    //     * 6 = SPORADIC, 7 = PATROL, 8 = TELEPORT
+    //     */
+    //public int[] mT;
+    //public Texture[] bg;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerCamera>().SetConstraints(x, y);
-        //Destroy(this);
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PlayerCamera>().SetConstraints(x, y);
+    //    //Destroy(this);
+    //}
+
+    /*
+     * turn into the scene loader
+     */
 }
