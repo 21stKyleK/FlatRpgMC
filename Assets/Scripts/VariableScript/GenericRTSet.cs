ad5e90f95f9977f8ddbb1e1c5e77f57469876ed5
21stKyleK <70537456+21stKyleK@users.noreply.github.com>
2021-01-28 20:58:29 -0600

    Update GenericRTSet.cs

--- Assets/Scripts/VariableScript/GenericRTSet.cs
+++ Assets/Scripts/VariableScript/GenericRTSet.cs
@@ -2,7 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GenericRTSet<T> : ScriptableObject
+public abstract class GenericRTSet<T> : ScriptableObject
 {
-    public List<T> things = new List<T>();
+    public List<T> Things = new List<T>();
+    
+    public void Add(T thing){
+        if(!Things.Contains(thing)){
+            Things.Add(thing);
+        }
+    }
+    
+    public void Remove(T thing){
+        if(Things.Contains(thing)){
+            Things.Remove(thing);
+        }
+    }
+    
+    /* in an extension for LoadingZones
+    include a method to reset the List
+    and a method that goes through the List to find an object with a !null collider
+    */
 }
