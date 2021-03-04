01e6c989b43586e9b830d9d43d5386b129b0137e
Kyle King <100040199@ccisd.net>
2021-03-04 09:51:51 -0600

    started battle backgrounds, 4 layers with plain color

--- Assets/Scripts/User Interface/ImageSO.cs
+++ Assets/Scripts/User Interface/ImageSO.cs
@@ -3,8 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[CreateAssetMenu (menuName = "Scriptable Variables/Layered Image")]
 public class ImageSO : ScriptableObject {
-    public Image Img;
 
+    public Color BackColor;
+    public Sprite Layer1, Layer2, Layer3;
 
+    public void SetColor(float r, float b, float g)
+    {
+        BackColor = new Color(r,b,g,1);
+    }
+
+    public void SetLayer1(Sprite l1)
+    {
+        Layer1 = l1;
+    }
+
+    public void SetLayer2(Sprite l2)
+    {
+        Layer2 = l2;
+    }
+
+    public void SetLayer3(Sprite l3)
+    {
+        Layer3 = l3;
+    }
+
+    public Sprite GetLayer(int i)
+    {
+        switch (i)
+        {
+            case (0):
+                return Layer1;
+            case (1):
+                return Layer2;
+            default:
+                return Layer3;
+        }
+    }
 }
