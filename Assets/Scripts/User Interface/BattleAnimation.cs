00e2974f0eb945868e298098676d20103a655c56
Kyle King <100040199@ccisd.net>
2021-03-09 09:52:12 -0600

    script uses base method instead of child

---
+++ Assets/Scripts/User Interface/BattleAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAnimation : PauseMenu {
+    
+    new public void DoAnim()
+    {
+
+    }
+
+    IEnumerator MovePause()
+    {
+        //cantPause.SetValue(true);
+
+        cantDo.SetValue(true);
+        float time = baseTime;
+        int cnt = 1;
+
+        while (time > 0)
+        {
+            if (!menuOut)
+            {
+                pauseMenu.transform.position = new Vector3(pauseMenu.transform.position.x, pauseMenu.transform.position.y + howFast / cnt++, pauseMenu.transform.position.z);
+            }
+            else
+            {
+                pauseMenu.transform.position = new Vector3(pauseMenu.transform.position.x, pauseMenu.transform.position.y - howFast / cnt++, pauseMenu.transform.position.z);
+            }
+            yield return null;
+            time -= Time.unscaledDeltaTime;
+            //time -= Time.deltaTime;
+
+            //Time.timeScale = 0;
+        }
+
+        if (menuOut)
+        {
+            pauseMenu.transform.position = new Vector3(pauseMenu.transform.position.x, inPos, pauseMenu.transform.position.z);
+            menuOut = false;
+        }
+        else
+        {
+            pauseMenu.transform.position = new Vector3(pauseMenu.transform.position.x, outPos, pauseMenu.transform.position.z);
+            menuOut = true;
+        }
+
+        cantDo.SetValue(false);
+
+        //cantPause.SetValue(false);
+    }
+}
---
+++ Assets/Scripts/User Interface/Pause/MenuAnim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class MenuAnim : MonoBehaviour
+{
+    public BoolVar cantDo;
+
+    public UnityEvent ButtonPress;
+
+    public void Handle()
+    {
+        //Debug.Log("Trigger");
+        if (!cantDo) { ButtonPress.Invoke(); }
+    }
+}
