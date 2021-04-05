f40eb36a35f022dee336ef44d144b67556fe3ab4
Kyle King <100040199@ccisd.net>
2021-04-05 15:26:24 -0500

    continued implementing fight structure

---
+++ Assets/Scripts/CombatStuff/Actions/Paralysis.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Paralysis : MonoBehaviour
+{
+    public int Turns;
+
+    //public static Image notif;
+    //might possibly use an SO list to pull from
+
+    //public Paralysis(int t)
+    //{
+
+    //}
+
+    //or Start
+    //public void Make()
+    //{
+
+    //    //create icon
+    //}
+
+    public virtual void Action()
+    {
+        Turns--;
+    }
+
+    public virtual void End()
+    {
+        //Destroy thing
+        Destroy(this);
+    }
+
+    public virtual void Already(int t)
+    {
+        Turns += t;
+    }
+}
